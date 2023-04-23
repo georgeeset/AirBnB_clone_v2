c3d26e4f28bfda78086836a12c896c19fd8ee854
georgeeset <georgeperfect4u@gmail.com>
2023-04-23 04:11:51 +0100

    C is fun

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+A script that starts a Flask web application
+"""
+
+from flask import Flask
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route('/')
+def index():
+    """ index """
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb')
+def hbnb():
+    """ hbnb route """
+    return "HBNB"
+
+
+@app.route('/c/<text>')
+def c(text):
+    """ /c route """
+    return "C %s" % text.replace("_", " ")
+
+
+if __name__ == '__main__':
+    app.run("0.0.0.0", 5000)
